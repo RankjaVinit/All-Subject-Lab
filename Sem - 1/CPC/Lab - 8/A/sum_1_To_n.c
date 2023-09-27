c82d1e8b214a95153c47a8228f08e499019bca6e
RankjaVinit <vinupatel1982@gmail.com>
2023-09-27 23:13:31 +0530

    Lab - 8 - A

--- Sem - 1/CPC/Lab - 8/A/sum_1_To_n.c
+++ Sem - 1/CPC/Lab - 8/A/sum_1_To_n.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 void main(){
-    int a;
-    
+    int n,i = 1,sum = 0;
+    printf("Enter Number : ");
+    scanf("%d",&n);
+    while(i <= n){
+        sum+=i;
+        i++;
+    }
+    printf("Sum = %d\n",sum);
 }
