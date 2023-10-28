43fde611b257af09187f4b200958cc8318de919a
RankjaVinit <vinupatel1982@gmail.com>
2023-10-28 20:48:56 +0530

    Lab - 05 - C

--- Sem - 1/CPC/Lab - 05/C/date_and_time.c
+++ Sem - 1/CPC/Lab - 05/C/date_and_time.c
@@ -6,5 +6,5 @@ void main()
     time_t t;   // not a primitive datatype
     time(&t);
 
-    printf("\nThis program has been writeen at (date and time): %s\n\n", ctime(&t));
+    printf("\nThis program has been started at (date and time): %s\n\n", ctime(&t));
 }
